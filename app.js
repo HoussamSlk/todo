802b6529a33d1c3494d7b3b8e282f1f2e45798c0
husamslk <qi.husam@gmail.com>
2018-04-17 23:06:52 +0200

    +

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var todoController = require('./controllers/todoController');
 var app = express();
-
+var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 
 todoController(app);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(port, function(){
   console.log('listening on', app.address().port);
 });
